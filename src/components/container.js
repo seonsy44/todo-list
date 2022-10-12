089be741a991db86e4572f35c3e130486d2bc2db
seon <seonsy44@gmail.com>
2022-10-12 15:34:27 +0900

    feat: create container

---
+++ src/components/container.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import { flexBox } from "../styles/mixins";
+
+const Container = ({ children }) => {
+  return (
+    <ContainerOuter>
+      <ContainerInner>{children}</ContainerInner>
+    </ContainerOuter>
+  );
+};
+
+export default Container;
+
+const ContainerOuter = styled.div`
+  width: 100vw;
+  height: 100vh;
+  ${flexBox()}
+`;
+
+const ContainerInner = styled.div`
+  width: 800px;
+  height: 600px;
+  padding: 30px;
+  box-sizing: border-box;
+  ${flexBox("column", "flex-start")};
+  overflow: scroll;
+  border-radius: 10px;
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.1);
+`;
