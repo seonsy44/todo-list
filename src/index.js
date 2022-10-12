1e907f1c2467b57c835bd6c92bac6f4a5cef5432
seon <seonsy44@gmail.com>
2022-10-12 15:22:04 +0900

    feat: create theme

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { ThemeProvider } from "styled-components";
+import { colors } from "./styles/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={colors}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/styles/theme.js
@@ -0,0 +1,7 @@
+export const colors = {
+  mainColor: "#023047",
+  pointColor: "#FFB703",
+  darkGrayColor: "#989898",
+  mediumGrayColor: "#E6E6E6",
+  lightGrayColor: "#FBFBFB",
+};
