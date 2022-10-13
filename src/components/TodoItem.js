dfe6a130a402dd97dc0725b029723f54e3e1b14b
seon <seonsy44@gmail.com>
2022-10-13 21:50:39 +0900

    feat: TodoItem

---
+++ src/components/TodoItem.js
@@ -0,0 +1,144 @@
+import styled, { css } from "styled-components";
+import { flexBox } from "../styles/mixins";
+import TodoInput from "./TodoInput";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { BsCheckLg } from "react-icons/bs";
+import { useState } from "react";
+import useUpdateTodo from "../hooks/useUpdateTodo";
+import useDeleteTodo from "../hooks/useDeleteTodo";
+
+const TodoItem = ({
+  todoItem: { id, todo, isCompleted },
+  handleTodoUpdate,
+  handleTodoDelete,
+}) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editValue, setEditValue] = useState(todo);
+
+  const updateTodo = useUpdateTodo();
+  const deleteTodo = useDeleteTodo();
+
+  const handleValueChange = (e) => setEditValue(e.target.value);
+
+  const handleEditClick = (e) => {
+    e.stopPropagation();
+    setIsEditing(true);
+  };
+
+  const handleCancelClick = () => {
+    setEditValue(todo);
+    setIsEditing(false);
+  };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    const updatedTodo = await updateTodo({ id, todo: editValue, isCompleted });
+    handleTodoUpdate(updatedTodo);
+    handleCancelClick();
+  };
+
+  const handleCompleteClick = async () => {
+    const updatedTodo = await updateTodo({
+      id,
+      todo,
+      isCompleted: !isCompleted,
+    });
+    handleTodoUpdate(updatedTodo);
+  };
+
+  const handleDeleteClick = async () => {
+    const success = await deleteTodo({ id });
+    if (success) handleTodoDelete(id);
+  };
+
+  if (isEditing)
+    return (
+      <form onSubmit={handleFormSubmit}>
+        <TodoInput
+          value={editValue}
+          onChange={handleValueChange}
+          name="editTodo"
+          buttonText="수정"
+          underLine
+          cancelBtn
+          onClickCancel={handleCancelClick}
+        />
+      </form>
+    );
+
+  return (
+    <TodoContainer onClick={handleCompleteClick}>
+      <SubContainer>
+        <Outlined isCompleted={isCompleted}>
+          <BsCheckLg />
+        </Outlined>
+        <Text isCompleted={isCompleted}>{todo}</Text>
+      </SubContainer>
+
+      <SubContainer>
+        <NotOutlined onClick={handleEditClick}>
+          <AiFillEdit />
+        </NotOutlined>
+        <NotOutlined onClick={handleDeleteClick}>
+          <AiFillDelete />
+        </NotOutlined>
+      </SubContainer>
+    </TodoContainer>
+  );
+};
+
+export default TodoItem;
+
+const TodoContainer = styled.div`
+  ${flexBox("row", "space-between", "center")};
+  width: 100%;
+  height: 50px;
+  border-radius: 10px;
+  margin-top: 1px;
+  cursor: pointer;
+  transition: all 200ms;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.tPointColor};
+  }
+`;
+
+const SubContainer = styled.div`
+  ${flexBox("row", "flex-start", "center")};
+`;
+
+const Text = styled.span`
+  font-size: 20px;
+  color: ${({ theme, isCompleted }) =>
+    isCompleted ? theme.darkGrayColor : theme.mainColor};
+  text-decoration: ${({ isCompleted }) => isCompleted && "line-through"};
+`;
+
+const IconContainer = css`
+  ${flexBox()};
+  margin-right: 7px;
+  width: 25px;
+  color: ${({ theme, isCompleted }) =>
+    isCompleted ? theme.pointColor : theme.darkGrayColor};
+  transition: all 200ms;
+`;
+
+const Outlined = styled.div`
+  ${IconContainer}
+  width: 18px;
+  height: 18px;
+  margin-left: 7px;
+  border: 1px solid
+    ${({ theme, isCompleted }) =>
+      isCompleted ? theme.pointColor : theme.darkGrayColor};
+  border-radius: 4px 4px 0 4px;
+  font-size: 10px;
+`;
+
+const NotOutlined = styled.div`
+  ${IconContainer}
+  font-size: 24px;
+  &:hover {
+    color: ${({ theme }) => theme.pointColor};
+  }
+`;
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,6 +1,7 @@
 export const colors = {
   mainColor: "#023047",
   pointColor: "#FFB703",
+  tPointColor: "rgba(255, 183, 3, 0.1)",
   darkGrayColor: "#989898",
   mediumGrayColor: "#E6E6E6",
   lightGrayColor: "#FBFBFB",
